b1a20827a9300fecaaa27c9cc9f5b7a8ddb435ac
Leviathenn <levixanderson@gmail.com>
2024-05-30 18:20:49 -0400

    fixed outlook url schema

--- background.js
+++ background.js
@@ -1,15 +1,8 @@
-chrome.runtime.onMessage.addListener(function(request) {
-    if (request.type === 'unsupported') {
-        chrome.tabs.create({
-            url: chrome.extension.getURL('webview/unsupported.html'),
-            active: false
-        }, function(tab) {
-            // After the tab has been created, open a window to inject the tab
-            chrome.windows.create({
-                tabId: tab.id,
-                type: 'popup',
-                focused: true
-            });
-        });
+// background.js
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'openPopup') {
+      chrome.action.openPopup();
+      sendResponse({status: 'Popup opened'});
     }
-});
+  }); 
--- contentScript.js
+++ contentScript.js
@@ -4,16 +4,32 @@
 
 function main(supported){
     console.log("asdasd")
-    chrome.runtime.sendMessage({type:'unsupported'})
-}
+    chrome.runtime.sendMessage({action: "openPopup"}, (response) => {
 
+    });
+}
+function unsupported(unsupported){
+
+}
 fetch("https://raw.githubusercontent.com/Leviathenn/SecureShark/main/manifests/secureShark.json").then((req)=>{
+    
     req.json().then((ssmanifest)=>{
+        ssmanifest["unsupported"].forEach((unsupportedd)=>{
+            fetch(`https://raw.githubusercontent.com/Leviathenn/SecureShark/manifests/manifest/${unsupportedd}`).then((cwd)=>{
+                cwd.json().then((mdv)=>{
+                    if(window.location.pathname == mdv["url"]){
+                        unsupported(mdv)
+                    }
+                }) 
+            })
+        });
         ssmanifest["supported"].forEach((supported)=>{
             fetch(`https://raw.githubusercontent.com/Leviathenn/SecureShark/main/manifests/${supported}.json`).then((cwd)=>{
-                if(window.location.pathname == supported["url"]){
-                    main(supported);
-                }
+                cwd.json().then((cnw)=>{
+                    if(window.location.pathname == cnw["url"]){
+                        main(cnw);
+                    }
+                })
             })
         })
     })
